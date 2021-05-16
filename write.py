bbbec35ca092bd4679edebf09cf04cbd081040bb
AlexanderB92 <a.m.brandstrup@gmail.com>
2021-05-16 12:00:00 +0200

    test

--- write.py
+++ write.py
@@ -8,7 +8,6 @@ workingfile = "store.txt"
 with open(datefile) as file, open(workingfile,'rb',buffering=0) as worker:
     for line in file:
         workingDate = line.rstrip()
-        time.sleep(1.0)
 
         blocksize = bytearray(128*1024)
         mem = memoryview(blocksize)
@@ -18,8 +17,8 @@ with open(datefile) as file, open(workingfile,'rb',buffering=0) as worker:
             algo.update(mem[:i])
 
         os.popen("echo \""+ algo.hexdigest() + "\" > a.txt")
-        os.popen("git add . && git commit --date=\"" + workingDate + "\" -m \"test\" && git push")
+        b = os.popen("git add . && git commit --date=\"" + workingDate + "\" -m \"test\" && git push")
 
-        print(workingDate)
+        print(workingDate + " " + b)
 
 
