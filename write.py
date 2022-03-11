321b70c19aed4a2bb12a2c549686b295a74dbf57
AlexanderB92 <a.m.brandstrup@gmail.com>
2022-03-11 11:05:18 +0100

    2nd test

---
+++ write.py
@@ -0,0 +1,26 @@
+import os
+import time
+import hashlib
+
+datefile = "dates.txt"
+workingfile = "a.txt"
+
+with open(datefile) as file, open(workingfile,'rb',buffering=0) as worker:
+    for line in file:
+        workingDate = line.rstrip()
+        time.sleep(1.0)
+
+        blocksize = bytearray(128*1024)
+        mem = memoryview(blocksize)
+        algo = hashlib.sha256()
+
+        for i in iter(lambda : worker.readinto(mem),0):
+            algo.update(mem[:i])
+
+        os.popen("echo \""+ algo.hexdigest() + "\" > a.txt")    
+        print("git add . && git commit --date='" + workingDate + " 12:00:00' -m \"test\" && git push")
+
+        
+        #os.popen("git add . && git commit --date='" + workingDate + " 12:00:00' -m \"test\" && git push")
+
+
